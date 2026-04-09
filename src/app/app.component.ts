9d2232ac118918c6fd4a5924f5684cda3b9b91df
Blanco German Alejandro <2437720@cegepmontpetit.ca>
2026-04-09 10:09:16 -0400

    FCT: Exercice animation 1 termine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import {transition, trigger, useAnimation} from "@angular/animations";
+import { bounce, jello, pulse, shakeX, tada } from 'ng-animate';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger("death", [
+      transition(":increment",
+        useAnimation(shakeX, {params: {timing: 0.5}})
+      )
+    ]),
+    trigger("attack", [
+      
+      transition(":increment", [
+        useAnimation(jello,{params: {timing: 0.3}} ),
+        useAnimation(pulse, {params: {timing: 0.3, scale: 4.5}})
+      ]),
+      
+    ])
+  ]
 })
 export class AppComponent {
   slimeIsPresent = false;
+  ng_death = 0;
+  ng_atack = 0;
+  css_hit = false;
+ 
 
   constructor() {
   }
@@ -16,6 +36,7 @@ export class AppComponent {
     this.showSlime();
     this.slimeIsPresent = true;
     // TODO Animation angular avec forwards
+    
 
   }
 
@@ -25,15 +46,22 @@ export class AppComponent {
     this.hideSlime();
 
     // TODO 2e animation angular en même temps
+    this.ng_death++;
   }
 
   attack(){
     // TODO Jouer une animation et augmenter l'intensité du mouvement avec scale
+    this.ng_atack++;
+
     // TODO Jouer une autre animation avant
   }
 
   hit(){
     // TODO Utilisé Animista pour faire une animation différente avec css (wobble)
+    this.css_hit = true;
+    setTimeout(() => {
+      this.css_hit = false;
+    }, 0.3 * 1000);
   }
 
   showSlime(){
